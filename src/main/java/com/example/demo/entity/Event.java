f1e217d1d3ac735a10de61e99fd016be37f2f01a
bala-amypo <bala@amypo.in>
2025-12-16 02:02:15 +0000

    User commit

--- src/main/java/com/example/demo/entity/Event.java
+++ src/main/java/com/example/demo/entity/Event.java
@@ -1,5 +1,9 @@
 package com.example.demo.entity;
 
 public class Event {
-    
+    private long id;
+    private String title;
+    private String description;
+    private String category;
+    private LocalDateT
 }
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,5 +1,49 @@
 package com.example.demo.entity;
 
 public class User {
+    private long id;
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+    public User(){
+        
+    }
+    public User(String name, String email, String password, String role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+    public long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getRole() {
+        return role;
+    }
     
 }
