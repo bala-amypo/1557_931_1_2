f48c30feb30e3365898a0e9c240c4bfe63a592e6
bala-amypo <bala@amypo.in>
2025-12-16 02:47:15 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,8 +1,12 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
@@ -12,5 +16,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 public class AuthController {
     
     @Autowired
+    UserService userservice;
     
+    @PostMapping("/register")
+    public User 
+
 }
--- src/main/java/com/example/demo/service/impl/UserServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/UserServiceimpl.java
@@ -2,11 +2,13 @@ package com.example.demo.service.impl;
 
 import org.springframework.stereotype.Service;
 
+import com.example.demo.entity.User;
 import com.example.demo.service.UserService;
 @Service
 public class UserServiceimpl implements UserService{
     
     public User saveUser(User user){
-        return u
+        return user;
     }
+    
 }
